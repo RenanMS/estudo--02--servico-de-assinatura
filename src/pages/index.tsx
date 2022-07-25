49de393c882de5d4387685c1601ed9afbb6d5a85
Renan Moreira <renan.bcc@gmail.com>
2022-07-24 21:49:02 -0300

    Título dinâmico por página

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
+
 import styles from '../styles/home.module.scss'
 
 export default function Home() {
   return (
-    <h1 className={styles.h1}>Limpando <span>estrutura</span></h1>
+    <>
+      <Head>
+        <title>Home | igNews - Serviço de assinatura</title>
+      </Head>
+      
+      <h1 className={styles.h1}>Limpando <span>estrutura</span></h1>
+    </>
   )
 }
