d727758198dca333175328756722e2c45fc2b062
Renan Moreira <renan.bcc@gmail.com>
2022-07-26 19:45:52 -0300

    Estilos globais

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,5 @@
 import Head from 'next/head'
 
-import styles from '../styles/home.module.scss'
-
 export default function Home() {
   return (
     <>
@@ -9,7 +7,7 @@ export default function Home() {
         <title>Home | igNews - Serviço de assinatura</title>
       </Head>
       
-      <h1 className={styles.h1}>Limpando <span>estrutura</span></h1>
+      <h1>Limpando <span>estrutura</span></h1>
     </>
   )
 }
